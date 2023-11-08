72690271937ed532bc2d3e5d62a0f0441d985de0
GonzoTheDev <c20703429@mytudublin.ie>
2023-11-08 19:28:57 +0000

    final

--- CA1/export.py
+++ CA1/export.py
@@ -1,12 +1,14 @@
 import mariadb
 import couchdb
+import uuid
 from datetime import datetime
 
 # Set up the connection to CouchDB
 couch = couchdb.Server("http://admin:couchdb@127.0.0.1:5984")  
 db_name = "music_comp_db"  # Replace with your desired CouchDB database name
 
-couch.delete(db_name)  # Delete the database if it already exists
+if db_name in couch:
+    couch.delete(db_name)  # Delete the database if it already exists
 
 try:
     db = couch.create(db_name)
@@ -29,7 +31,60 @@ db_config = {
 conn = mariadb.connect(**db_config)
 cursor = conn.cursor(dictionary=True)
 
+# Define the design document with all views
+design_doc = {
+    "_id": "_design/views",
+    "views": {
+        "by_county_and_participant": {
+            "map": "function (doc) { if (doc.type === 'votes_fact' && doc.edition.edyear === 2022 && doc.vote_category.catid === 2) { emit([doc.participant.countyname, doc.participant.partname], doc.vote); }}"
+        },
+        "by_viewer": {
+            "map": "function (doc) { if (doc.type === 'viewers_dim') { emit(doc._id, doc.age_groupid, doc.viewerid); }}"
+        },
+        "by_county": {
+            "map": "function (doc) { if (doc.type === 'county_dim') { emit(doc._id, doc.countyid, doc.countyname); }}"
+        },
+        "by_edition": {
+            "map": "function (doc) { if (doc.type === 'edition_dim') { emit(doc._id, doc.edyear, doc.edpresenter); }}"
+        },
+        "by_participants": {
+            "map": "function (doc) { if (doc.type === 'participants_dim') { emit(doc._id, doc.partname, doc.countyid); }}"
+        },
+        "by_viewercategory": {
+            "map": "function (doc) { if (doc.type === 'viewercategory_dim') { emit(doc._id, doc.catid, doc.catname); }}"
+        }
+    }
+}
+
+# Save the design document
+try:
+    db[design_doc["_id"]] = design_doc
+    print("Design document created.")
+except couchdb.ResourceConflict:
+    print("Design document already exists.")
+
+    
+def get_document_id_by_key(db, key, type, view):
+    search_key = type + "_" + str(key)
+    view_result = db.view('views/' + view, key=search_key)
+    
+    if view_result:
+        return view_result.rows[0].id
+    else:
+        return None
+
+def get_countyname(db, key, type, view):
+    search_key = type + "_" + str(key)
+    view_result = db.view('views/' + view, key=search_key)
+    
+    if view_result:
+        return view_result.rows[0].countyname
+    else:
+        return None
+
+
 # Define tables to export
+#  
 tables = ['AGEGROUP_DIM', 'COUNTY_DIM', 'EDITION_DIM', 'PARTICIPANTS_DIM', 'VIEWERS_DIM', 'VIEWERCATEGORY_DIM', 'VOTES_FACT']
 
 for table in tables:
@@ -41,129 +96,129 @@ for table in tables:
     for row in rows:
         # Build CouchDB document structure with a timestamp to ensure uniqueness
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
-        document_id = f"{table.lower()}_{row.get(f'{table.upper()}_ID', row.get(f'{table.upper()}ID'))}_{timestamp}"
-        document = {"_id": document_id, "type": table.lower()}
+        
+        viewer_id = row.get('VIEWERID', '')
+        age_group_id = row.get('AGE_GROUPID', '')
+        age_group_desc = row.get('AGE_GROUP_DESC', '')
+        ed_year = row.get('EDYEAR', '')
+        ed_presenter = row.get('EDPRESENTER', '')
+        county_id = row.get('COUNTYID', '')
+        county_name = row.get('COUNTYNAME', '')
+        participant_name = row.get('PARTNAME', '')
+        viewer_category_id = row.get('CATID', '')
+        viewer_category_name = row.get('CATNAME', '')
+        vote_category_id = row.get('VOTE_CATEGORY', '')
+        vote_mode = row.get('VOTEMODE', '')
+        vote = row.get('VOTE', '')
+        vote_id = row.get('VOTE_ID', '')
+
+        viewer_document_id = f"viewers_dim_{viewer_id}"
+        age_group_document_id = f"agegroup_dim_{age_group_id}"
+        county_document_id = f"county_dim_{county_id}"
+        participant_document_id = f"participants_dim_{participant_name.lower()}"
+        viewer_category_document_id = f"viewercategory_dim_{viewer_category_id}"
+        edition_document_id = f"edition_dim_{ed_year}_{ed_presenter}"
+        votes_fact_document_id = f"votes_fact_{vote_id}"
+        
+        if table == 'AGEGROUP_DIM':
+            document = {"_id": age_group_document_id, "type": table.lower()}
+        if table == 'COUNTY_DIM':
+            document = {"_id": county_document_id, "type": table.lower()}
+        if table == 'EDITION_DIM':
+            document = {"_id": edition_document_id, "type": table.lower()}
+        if table == 'PARTICIPANTS_DIM':
+            document = {"_id": participant_document_id, "type": table.lower()}
+        if table == 'VIEWERS_DIM':
+            document = {"_id": viewer_document_id, "type": table.lower()}
+        if table == 'VIEWERCATEGORY_DIM':
+            document = {"_id": viewer_category_document_id, "type": table.lower()}
+        if table == 'VOTES_FACT':
+            document = {"_id": votes_fact_document_id, "type": table.lower()}
+
 
         # Add table-specific fields
         if table == 'AGEGROUP_DIM':
             document.update({
-                "age_groupid": row['AGE_GROUPID'],
-                "age_group_desc": row['AGE_GROUP_DESC']
+                "_id": age_group_document_id,
+                "age_groupid": age_group_id,
+                "age_group_desc": age_group_desc
             })
+            print(f"Exporting {table} document with ID {age_group_document_id}")
         elif table == 'COUNTY_DIM':
             document.update({
-                "countyid": row['COUNTYID'],
-                "countyname": row['COUNTYNAME']
+                "_id": county_document_id,
+                "countyid": county_id,
+                "countyname": county_name
             })
+            print(f"Exporting {table} document with ID {county_document_id}")
         elif table == 'EDITION_DIM':
             document.update({
-                "edyear": row['EDYEAR'],
-                "edpresenter": row['EDPRESENTER']
+                "_id": edition_document_id,
+                "edyear": ed_year,
+                "edpresenter": ed_presenter
             })
+            print(f"Exporting {table} document with ID {edition_document_id}")
         elif table == 'PARTICIPANTS_DIM':
             document.update({
-                "partname": row['PARTNAME'],
-                "countyid": row['COUNTYID']
+                "_id": participant_document_id,
+                "partname": participant_name,
+                "countyid": county_id
             })
+            pt_countyname_document_id = get_countyname(db, county_id, 'county_dim', 'by_viewer')
+            if(pt_countyname_document_id):
+                document['countyname'] = pt_countyname_document_id
+            print(f"Exporting {table} document with ID {participant_document_id}")
         elif table == 'VIEWERS_DIM':
             document.update({
-                "viewerid": row['VIEWERID'],
-                "age_groupid": row['AGE_GROUPID'],
-                "countyid": row['COUNTYID']
+                "_id": viewer_document_id,
+                "viewerid": viewer_id,
+                "age_groupid": age_group_id,
+                "countyid": county_id
             })
+            print(f"Exporting {table} document with ID {viewer_document_id}")
         elif table == 'VIEWERCATEGORY_DIM':
-            document.update({
-                "catid": row['CATID'],
-                "catname": row['CATNAME']
-            })
+                document.update({
+                    "_id": viewer_category_document_id,
+                    "catid": viewer_category_id,
+                    "catname": viewer_category_name
+                })
+                print(f"Exporting {table} document with ID {viewer_category_document_id}")
         elif table == 'VOTES_FACT':
-            viewer_id = row.get('VIEWERID', '')
-            age_group_id = row.get('AGE_GROUPID', '')
-            county_id = row.get('COUNTYID', '')
-            participant_name = row.get('PARTNAME', '')
-            vote_category_id = row.get('VOTE_CATEGORY', '')
-            vote_category_name = row.get('CATNAME', '')
-
-            viewer_document_id = f"viewers_dim_{viewer_id}"
-            age_group_document_id = f"agegroup_dim_{age_group_id}"
-            county_document_id = f"county_dim_{county_id}"
-            participant_document_id = f"participants_dim_{participant_name}"
-            vote_category_document_id = f"viewercategory_dim_{vote_category_id}"
-
             document.update({
+                "_id": votes_fact_document_id,
                 "vote_id": row['VOTE_ID'],
                 "viewer": {"id": viewer_id, "age_groupid": age_group_id, "countyid": county_id},
-                "edition": {"edyear": row['EDYEAR'], "edpresenter": row.get('ED_PRESENTER', '')},
+                "edition": {"edyear": row['EDYEAR']},
                 "participant": {"partname": participant_name, "countyid": county_id},
-                "vote_category": {"catid": vote_category_id, "catname": row.get('CATNAME', '')},
-                "votemode": row.get('VOTEMODE', ''),
-                "vote": row.get('VOTE', None)
+                "vote_category": {"catid": vote_category_id},
+                "votemode": vote_mode,
+                "vote": vote
             })
 
-            print(f"Exporting {table} document with ID {document_id}")
+            vf_viewer_document_id = get_document_id_by_key(db, viewer_id, 'viewers_dim', 'by_viewer')
+            if(vf_viewer_document_id):
+                document['viewer']['_id'] = vf_viewer_document_id
+            
+            vf_edition_document_id = get_document_id_by_key(db, ed_year, '', 'by_edition')
+            if(vf_edition_document_id):
+                document['edition']['_id'] = vf_edition_document_id
 
-            # Link the nested documents
-            age_group_document_id = f"agegroup_dim_{age_group_id}"
-            county_document_id = f"county_dim_{county_id}"
-            viewer_document_id = f"viewers_dim_{viewer_id}"
-            participant_document_id = f"participants_dim_{participant_name}"
-            vote_category_document_id = f"viewercategory_dim_{vote_category_id}"
+            vf_participant_document_id = get_document_id_by_key(db, participant_name.lower(), 'participants_dim', 'by_participants')
+            if(vf_participant_document_id):
+                document['participant']['_id'] = vf_participant_document_id
 
-            # Save the linked documents if not already present
-            if age_group_document_id not in db:
-                print(f"AGEGROUP_DIM document with ID {age_group_document_id} not found. Creating...")
-                age_group_document = {
-                    "_id": age_group_document_id,
-                    "type": "agegroup_dim",
-                    "age_groupid": age_group_id,
-                    "age_group_desc": f"Age Group {age_group_id}"
-                }
-                db.save(age_group_document)
+            vf_vote_category_document_id = get_document_id_by_key(db, vote_category_id, 'viewercategory_dim', 'by_viewercategory')
+            if(vf_vote_category_document_id):
+                document['vote_category']['_id'] = vf_vote_category_document_id
+                
 
-            if county_document_id not in db:
-                print(f"COUNTY_DIM document with ID {county_document_id} not found. Creating...")
-                county_document = {
-                    "_id": county_document_id,
-                    "type": "county_dim",
-                    "countyid": county_id,
-                    "countyname": f"County {county_id}"
-                }
-                db.save(county_document)
-
-            if viewer_document_id not in db:
-                print(f"VIEWERS_DIM document with ID {viewer_document_id} not found. Creating...")
-                viewer_document = {
-                    "_id": viewer_document_id,
-                    "type": "viewers_dim",
-                    "viewerid": viewer_id,
-                    "age_groupid": age_group_id,
-                    "countyid": county_id
-                }
-                db.save(viewer_document)
-
-            if participant_document_id not in db:
-                print(f"PARTICIPANTS_DIM document with ID {participant_document_id} not found. Creating...")
-                participant_document = {
-                    "_id": participant_document_id,
-                    "type": "participants_dim",
-                    "partname": participant_name,
-                    "countyid": county_id
-                }
-                db.save(participant_document)
-
-            if vote_category_document_id not in db:
-                print(f"VIEWERCATEGORY_DIM document with ID {vote_category_document_id} not found. Creating...")
-                vote_category_document = {
-                    "_id": vote_category_document_id,
-                    "type": "viewercategory_dim",
-                    "catid": vote_category_id,
-                    "catname": vote_category_name
-                }
-                db.save(vote_category_document)
-
+            print(f"Exporting {table} document with ID {votes_fact_document_id}")
 
         # Insert the document into CouchDB
         db.save(document)
+
+
+
 
 # Close the database connections
 cursor.close()
